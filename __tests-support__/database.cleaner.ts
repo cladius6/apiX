9dec3dabb2afa983c6adc80b18f3cabf8562dd6f
Cladius <klaudiusz.kowalski@elpassion.pl>
2023-04-22 17:27:24 +0200

    Add database cleaner

---
+++ __tests-support__/database.cleaner.ts
@@ -0,0 +1,32 @@
+import { INestApplication, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../src/prisma/prisma.service';
+
+@Injectable()
+export class DatabaseCleaner {
+  constructor(private prismaService: PrismaService) {}
+
+  public async cleanup() {
+    const tables = Prisma.dmmf.datamodel.models
+      .map(model => model.dbName || model.name)
+      .filter(name => name !== '_prisma_migrations');
+
+      console.log({tables})
+      console.log(`TRUNCATE TABLE ${tables.join(', ')} RESTART IDENTITY CASCADE`)
+
+    return this.prismaService.$executeRawUnsafe(
+      `TRUNCATE TABLE "${tables.join(', ')}" RESTART IDENTITY CASCADE`,
+    );
+  }
+}
+
+export const clearDatabase = async (app: INestApplication): Promise<void> => {
+  const databaseCleaner = app.get(DatabaseCleaner);
+  await databaseCleaner.cleanup();
+};
+
+export const cleanupDbBeforeEach = (appGetter: () => INestApplication) => {
+  beforeEach(async () => {
+    await clearDatabase(appGetter());
+  });
+};
