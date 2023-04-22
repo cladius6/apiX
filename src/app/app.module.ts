67d49ce346dfa38edd1fa5b30d94cea19ad2a4c9
Cladius <klaudiusz.kowalski@elpassion.pl>
2023-04-22 17:28:09 +0200

    Update app module & blog module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BlogModule } from '../blog/blog.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [AppModule],
+  imports: [AppModule, PrismaModule, BlogModule ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [BlogController],
-  providers: [BlogService]
+  providers: [BlogService, PrismaService]
 })
 export class BlogModule {}
